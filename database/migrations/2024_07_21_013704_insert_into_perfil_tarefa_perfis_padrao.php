e6380e5c3c36392f259bc74e29cef11c282b1ddd
Matheus Vinicius <matheusv.16santos@gmail.com>
2024-07-20 23:01:24 -0300

    feat: Adiciona dados padroes na tabela perfil tarefa

---
+++ database/migrations/2024_07_21_013704_insert_into_perfil_tarefa_perfis_padrao.php
@@ -0,0 +1,27 @@
+<?php
+
+use \Illuminate\Database\Migrations\Migration;
+use \App\Packages\PerfilTarefa\Actions\PerfilTarefaAction;
+use \Mviniis\ConnectionDatabase\SQL\Parts\{SQLValues, SQLValuesGroup};
+
+return new class extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		define('PATH_ENV_APP', __DIR__ . "/../../.env");
+
+		(new PerfilTarefaAction)->insert(
+			ignore: true,
+			values: new SQLValues([
+				new SQLValuesGroup([1, 'Admin', 's', 's', 's']),
+				new SQLValuesGroup([2, 'Editor', 's', 's', 'n']),
+				new SQLValuesGroup([3, 'Observador', 's', 'n', 'n'])
+			])
+		)->rowCount();
+	}
+};
