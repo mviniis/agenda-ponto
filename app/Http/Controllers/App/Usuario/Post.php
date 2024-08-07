c765fb10ca7e2157f51b432cd6d6d2337509aaa3
Matheus Vinicius <matheusv.16santos@gmail.com>
2024-08-06 21:39:46 -0300

    feat: Adiciona controlador para a atualizacao do perfil do usuario

---
+++ app/Http/Controllers/App/Usuario/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\App\Usuario;
+
+use \Illuminate\Http\Request;
+use \App\Http\Controllers\Framework\Base;
+
+/**
+ * class Post
+ * 
+ * Classe responsável por controlar as requisições POST da página de atualização do perfil do usuário
+ * 
+ * @author Matheus Vinicius
+ */
+class Post extends Base {
+  public function configure(): self {
+    return $this;
+  }
+
+  /**
+   * Método responsável por realizar a validação da requisição de login
+   * @param  Request      $request      Dados da requisição
+   * @return string
+   */
+  public function atualizar(Request $request, $id = null) {
+    return response()->json([
+      'status'   => true,
+      'mensagem' => 'Perfil atualizado com sucesso!'
+    ]);
+  }
+}
