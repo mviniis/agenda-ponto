35c54cf7286386e4afbd674e26e10efd9640ba74
Matheus Vinicius <matheusv.16santos@gmail.com>
2024-08-14 12:21:54 -0300

    feat: Adiciona middleware para validar o usuario logado

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use \Illuminate\Support\Facades\Route;
+use \App\Http\Middleware\App\{AppRequireLogin};
 
 /*
 |--------------------------------------------------------------------------
@@ -13,58 +14,64 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [
-  \App\Http\Controllers\App\Inicio\Get::class, 'consultar'
-])->name('web.ver.login');
+// REQUISIÇÕES QUE RETORNARÃO UMA PÁGINA
+Route::middleware([AppRequireLogin::class])->group(function() {
+  Route::get('/', [
+    \App\Http\Controllers\App\Inicio\Get::class, 'consultar'
+  ])->name('web.ver.login');
 
-Route::post('/', [
-  \App\Http\Controllers\App\Inicio\Post::class, 'efetuarLogin'
-])->name('request.logar');
+  Route::get('/cadastro', [
+    \App\Http\Controllers\App\Cadastro\Get::class, 'consultar'
+  ])->name('web.ver.cadastro');
 
-Route::get('/cadastro', [
-  \App\Http\Controllers\App\Cadastro\Get::class, 'consultar'
-])->name('web.ver.cadastro');
+  Route::get('/recuperar-senha', [
+    \App\Http\Controllers\App\Recuperacao\Get::class, 'consultar'
+  ])->name('web.ver.recuperacao');
 
-Route::post('/cadastro', [
-  \App\Http\Controllers\App\Cadastro\Post::class, 'cadastrarUsuario'
-])->name('request.cadastrar');
+  Route::get('/listagem-tarefas', [
+    \App\Http\Controllers\App\Listagem\Get::class, 'consultar'
+  ])->name('web.ver.listagem');
+  
+  Route::get('/cadastrar-tarefa', [
+    \App\Http\Controllers\App\Tarefa\Get::class, 'consultar'
+  ])->name('web.ver.tarefa');
+  
+  Route::get('/editar-tarefa/{id}', [
+    \App\Http\Controllers\App\Tarefa\Get::class, 'consultar'
+  ])->name('web.ver.tarefa.detalhe');
+  
+  Route::get('/editar-perfil', [
+    \App\Http\Controllers\App\Usuario\Get::class, 'consultar'
+  ])->name('web.ver.perfil');
+});
 
-Route::get('/recuperar-senha', [
-  \App\Http\Controllers\App\Recuperacao\Get::class, 'consultar'
-])->name('web.ver.recuperacao');
-
-Route::post('/recuperar-senha', [
-  \App\Http\Controllers\App\Recuperacao\Post::class, 'recuperarSenha'
-])->name('request.recuperacao');
-
-Route::get('/listagem-tarefas', [
-  \App\Http\Controllers\App\Listagem\Get::class, 'consultar'
-])->name('web.ver.listagem');
-
-Route::get('/cadastrar-tarefa', [
-  \App\Http\Controllers\App\Tarefa\Get::class, 'consultar'
-])->name('web.ver.tarefa');
-
-Route::post('/cadastrar-tarefa', [
-  \App\Http\Controllers\App\Tarefa\Post::class, 'cadastrarAtualizarTarefa'
-])->name('request.tarefa');
-
-Route::get('/editar-tarefa/{id}', [
-  \App\Http\Controllers\App\Tarefa\Get::class, 'consultar'
-])->name('web.ver.tarefa.detalhe');
-
-Route::post('/editar-tarefa', [
-  \App\Http\Controllers\App\Tarefa\Post::class, 'cadastrarAtualizarTarefa'
-])->name('request.tarefa.detalhe');
-
-Route::delete('/remover-tarefa', [
-  \App\Http\Controllers\App\Tarefa\Delete::class, 'remover'
-])->name('request.tarefa.detalhe.remocao');
-
-Route::get('/editar-perfil', [
-  \App\Http\Controllers\App\Usuario\Get::class, 'consultar'
-])->name('web.ver.perfil');
-
-Route::post('/editar-perfil', [
-  \App\Http\Controllers\App\Usuario\Post::class, 'atualizar'
-])->name('request.perfil');
+// REQUISIÇÕES HTTP QUE REALIZARÃO UMA AÇÃO E NÃO VÃO RETORNAR HTML
+Route::middleware([])->group(function() {
+  Route::post('/', [
+    \App\Http\Controllers\App\Inicio\Post::class, 'efetuarLogin'
+  ])->name('request.logar');
+  
+  Route::post('/cadastro', [
+    \App\Http\Controllers\App\Cadastro\Post::class, 'cadastrarUsuario'
+  ])->name('request.cadastrar');
+  
+  Route::post('/recuperar-senha', [
+    \App\Http\Controllers\App\Recuperacao\Post::class, 'recuperarSenha'
+  ])->name('request.recuperacao');
+  
+  Route::post('/cadastrar-tarefa', [
+    \App\Http\Controllers\App\Tarefa\Post::class, 'cadastrarAtualizarTarefa'
+  ])->name('request.tarefa');
+  
+  Route::post('/editar-tarefa', [
+    \App\Http\Controllers\App\Tarefa\Post::class, 'cadastrarAtualizarTarefa'
+  ])->name('request.tarefa.detalhe');
+  
+  Route::delete('/remover-tarefa', [
+    \App\Http\Controllers\App\Tarefa\Delete::class, 'remover'
+  ])->name('request.tarefa.detalhe.remocao');
+  
+  Route::post('/editar-perfil', [
+    \App\Http\Controllers\App\Usuario\Post::class, 'atualizar'
+  ])->name('request.perfil');
+});
