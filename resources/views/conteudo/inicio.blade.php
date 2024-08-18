a73aed9e1cd40f353ac3d41766ba1a2a9299c248
João Gabriel <joaogabriel.gomesvieirag8@gmail.com>
2024-08-18 12:59:35 -0300

    Adição tela de cadastro de usuário

--- resources/views/conteudo/inicio.blade.php
+++ resources/views/conteudo/inicio.blade.php
@@ -14,18 +14,22 @@
                     <input type="email" class="form-control" id="floating1" placeholder="name@example.com">
                     <label for="floating1">Usuário</label>
                 </div>
-                <div class="form-floating">
-                    <input type="password" class="form-control" id="floating1" placeholder="Password">
-                    <label for="floating1"><img src="{{ $URL_IMG }}/geral/cadeadosenha.svg" style="width: 20px; height: auto; padding-right: 5px;"
-                    alt="AgendaPonto Logo" class="w">Senha</label>
+                <div class="form-floating position-relative">
+                    <input type="password" class="form-control" id="password" placeholder="Password">
+                    <label for="password"><img src="{{ $URL_IMG }}/geral/cadeadosenha.svg"
+                            style="width: 20px; height: auto; padding-right: 5px;" alt="AgendaPonto Logo"
+                            class="w">Senha</label>
+                    <span onclick="togglePasswordVisibility()" style="cursor: pointer; position: absolute; right: 10px; top: 50%; transform: translateY(-50%);">
+                        <img id="toggleIcon" src="https://img.icons8.com/ios-glyphs/30/000000/visible.png" alt="Mostrar senha" />
+                    </span>
                 </div>
                 <div class="esqueceu-senha">
-                    <a href="#">Esqueceu a senha?</a>
+                    <a href="{{ $URL_APP }}/recuperar-senha">Esqueceu a senha?</a>
                 </div>
                 <button type="submit">ENTRAR</button>
             </form>
             <div class="criar-conta">
-                <p>Não tenho conta? <a href="#">Criar conta agora</a></p>
+                <p>Não tenho conta? <a href="{{ $URL_APP }}/cadastro">Criar conta agora</a></p>
             </div>
         </div>
     </div>
--- resources/views/estrutura/head.blade.php
+++ resources/views/estrutura/head.blade.php
@@ -5,11 +5,14 @@
   <title>{{ $TITLE_SITE }}</title>
 
   <!-- MATERIAL ICONS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" rel="stylesheet" />
   <link rel="stylesheet" href="{{ $linkCss }}">
   <link rel="shortcut icon" href="{{ $URL_IMG }}/geral/favicon.png" type="image/x-icon">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+    rel="stylesheet">
 </head>
