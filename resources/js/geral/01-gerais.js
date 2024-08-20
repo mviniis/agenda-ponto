158597342821d2ae1882793c9bbdcecb8cc219b2
Matheus Vinicius <matheusv.16santos@gmail.com>
2024-08-19 22:26:44 -0300

    fix: Ajuste no CSRF token

--- resources/js/geral/01-gerais.js
+++ resources/js/geral/01-gerais.js
@@ -1,13 +1,19 @@
-// DEFINE O CSRF TOKEN PARA TODAS AS REQUISIÇÕES AJAX
-$.ajaxSetup({
-  headers: {
-    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-  }
+$(() => {
+  console.log('teste 1');
+  
+  // DEFINE O CSRF TOKEN PARA TODAS AS REQUISIÇÕES AJAX
+  $.ajaxSetup({
+    headers: {
+      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+  });
 });
 
 // LOGOUT DO USUÁRIO
 $(function() {
   $(document).on('click', '[data-action-logout]', () => {
+    console.log('teste 2');
+    
     $.ajax({
       url: URL_APP + '/logout',
       method: 'POST',
