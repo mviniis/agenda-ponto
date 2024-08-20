af58c43c4de7167451147f21c6e1f3ec4654cfa4
GabrielOliveira15 <gabrielnascimento751@outlook.com>
2024-08-19 22:16:48 -0300

    Estilização editar tarefa

--- resources/views/conteudo/editar-tarefa.blade.php
+++ resources/views/conteudo/editar-tarefa.blade.php
@@ -1,4 +1,4 @@
-<h1>Editar uma tarefa</h1>
+<!--<h1>Editar uma tarefa</h1>
 
 <a href="{{ $URL_APP }}/listagem-tarefas">Voltar</a>
 
@@ -28,3 +28,50 @@
 
   <button type="submit">Enviar</button>
 </form>
+-->
+<div class="cabecalho">
+  <div class="cabecalho_conteudo">
+    <a href="{{ $URL_APP }}/listagem-tarefas"" class="cabecalho_conteudo_sair"><strong>Sair</strong></a>
+  </div>
+</div>
+
+
+<div class="container py-5 conteudo_form">
+  <div class="conteudo_cabecalho_cabecalho">
+    <div class="conteudo_cabecalho">
+      <h1><strong>Editar tarefa</strong></h1>
+    </div>
+    <a href="{{ $URL_APP }}/listagem-tarefas" data-placement="top" title="Voltar"><img src="{{ $URL_IMG }}/geral/fechar.png" class="conteudo_sair" alt="Fechar Icone" class="w"></a>
+  </div>
+
+  <form action="{{ $URL_APP }}/cadastrarAtualizarTarefa" method="post">
+    @csrf
+
+    <div class="conteudo_form_container">
+      <p>Nome da tarefa</p>
+      <input type="text" name="nomeTarefa" id="nomeTarefa" title="nomeTarefa" class="conteudo_form_campo" placeholder="Nome da tarefa" autocomplete="off" required>
+    </div>
+
+    <div class="conteudo_form_container">
+      <p>Prioridade</p>
+      <input type="radio" class="btn-check" name="definirPrioridade" id="baixa" autocomplete="off">
+      <label class="btn check_baixa" for="baixa">Baixa</label>
+
+      <input type="radio" class="btn-check" name="definirPrioridade" id="media" autocomplete="off">
+      <label class="btn check_media" for="media">Média</label>
+
+      <input type="radio" class="btn-check" name="definirPrioridade" id="alta" autocomplete="off">
+      <label class="btn check_alta" for="alta">Alta</label>
+    </div>
+
+    <div class="conteudo_form_container">
+      <p>Descrição</p>
+      <input type="text" class="conteudo_form_campo_descricao" placeholder="Escreva aqui..." name="descricao"></input>
+    </div>
+
+    <div class="conteudo_form_container btn_enviar_container">
+      <button type="submit" class="btn_enviar">SALVAR</button>
+    </div>
+  </form>
+</div>
+<div class="conteudo_footer"></div>
--- resources/views/conteudo/senha-alterada.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="corpo_pagina">
-    <div class="cabecalho"></div>
-    <div class="conteudo">
-        <img src="{{ $URL_IMG }}/geral/agendaponto-simb.svg" class="logo_formatada">
-        <h1><strong>Senha alterada!</strong></h1>
-        <a href="{{ $URL_APP }}" class="btn btn_formatado">ENTRAR</a>
-    </div>
-    <div class="rodape"></div>
-</div>
